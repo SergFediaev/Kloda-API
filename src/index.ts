3f47fb18ded47ba5e56367ccffef59fe948aba90
SergFediaev <sergfediaev@gmail.com>
2024-09-16 17:06:35 +0300

    init: elysia-rate-limit

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,26 @@
-import { Elysia } from "elysia";
+import { Elysia } from 'elysia'
+import { rateLimit } from 'elysia-rate-limit'
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const app = new Elysia()
+	.use(
+		rateLimit({
+			max: 3,
+			duration: 90_000,
+			errorResponse: new Response(
+				'rate-limited: No more trolling, young hacker ;D',
+				{
+					status: 429,
+					headers: new Headers({
+						'Content-Type': 'text/plain',
+						'Custom-Header': 'custom',
+					}),
+				},
+			),
+		}),
+	)
+	.get('/', () => 'Hello Kloda  ♠ ♣ ♤ ♧ 🃏')
+	.listen(3_000)
 
 console.log(
-  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
-);
+	`🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`,
+)
