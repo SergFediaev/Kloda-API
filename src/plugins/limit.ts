d9174f82e6c8e84fc3e331c0ea3a1e1048312f75
SergFediaev <sergfediaev@gmail.com>
2024-11-06 18:44:42 +0300

    refactor: rate-limit max requests duration

--- src/plugins/limit.ts
+++ src/plugins/limit.ts
@@ -1,8 +1,14 @@
 import { Elysia } from 'elysia'
 import { rateLimit } from 'elysia-rate-limit'
 
+const duration = Number(process.env.REQUESTS_DURATION)
+
+const max = Number(process.env.MAXIMUM_REQUESTS)
+
 export const limitPlugin = new Elysia().use(
   rateLimit({
+    duration,
+    max,
     errorResponse: new Response('rate-limited: Too many requests', {
       status: 429,
       headers: new Headers({
